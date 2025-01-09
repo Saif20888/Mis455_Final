7b4424df07cc8831f593e3301c1d19df1f13c7e8
saifs@Saif <mohammadshahriar95@gmail.com>
2025-01-10 02:33:56 +0600

    sixth commit

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const resultsContainer = document.getElementById("results-container");
+const showAllButton = document.getElementById("show-all");
   
