b2558230a32da3000e38f75b73e359a11ac66acf
saifs@Saif <mohammadshahriar95@gmail.com>
2025-01-10 03:06:45 +0600

    eighth commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const searchButton = document.getElementById("search-button");
-const searchInput = document.getElementById("search-input");
-const resultsContainer = document.getElementById("results-container");
-const showAllButton = document.getElementById("show-all");
-  
+var searchButton = document.getElementById("search-button");
+var searchInput = document.getElementById("search-input");
+var resultsContainer = document.getElementById("results-container");
+var showAllButton = document.getElementById("show-all");
+
+searchButton.addEventListener("click", function() {
+  var query = searchInput.value.trim();
+  if (!query) return;
