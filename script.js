a1ce45bdd38622859d1f06b4f8efc85fed5328b6
saifs@Saif <mohammadshahriar95@gmail.com>
2025-01-10 03:21:30 +0600

    ninth commit

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ var showAllButton = document.getElementById("show-all");
 searchButton.addEventListener("click", function() {
   var query = searchInput.value.trim();
   if (!query) return;
+
+   
+   resultsContainer.innerHTML = "";
+   showAllButton.style.display = "none";
+ 
+   
+   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+     .then(function(response) {
+       return response.json();
+     })
+     .then(function(data) {
+       if (data.meals) {
+         displayMeals(data.meals);
+       } else {
+         resultsContainer.innerHTML = "<p>No meals found!</p>";
+       }
+     })
+     .catch(function(error) {
+       console.error("Error fetching data:", error);
+     });
+ });
+ 
+ 
+
